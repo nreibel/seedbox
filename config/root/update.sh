a5f6f967cb4bf315451dd8a9395a7f4c6d97ea95
Nicolas R <nreibel@users.noreply.github.com>
2020-12-29 19:44:08 +0100

    Update transmission-rpc as well

--- config/root/update.sh
+++ config/root/update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 apt update && apt-get dist-upgrade -y
 apt autoremove
-apt autoclean
+apt clean
 /root/flexget/bin/pip install --upgrade setuptools
 /root/flexget/bin/pip install --upgrade flexget
+/root/flexget/bin/pip install --upgrade transmission-rpc
 reboot
